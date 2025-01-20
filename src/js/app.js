8da7db19ae3e744a8c552e11d3633e29b3736cd2
jerzy szajner <jerzy.szajner@gmail.com>
2025-01-20 09:11:40 +0100

    add initial render to renderNotes

--- src/js/app.js
+++ src/js/app.js
@@ -7,6 +7,9 @@ const subjectInput = document.querySelector(".form__subject-input");
 const dateInput = document.querySelector(".form__date-input");
 const noteText = document.querySelector(".form__note-input");
 
+// Initial render
+document.addEventListener("DOMContentLoaded", renderNotes);
+
 // Add submit event to the form
 form.addEventListener("submit", (e) => {
   e.preventDefault();
